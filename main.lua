f93af5746cac8ecd891566f980f6580f2e35022d
Pedro Sousa <pedromontsousa@gmail.com>
2024-12-10 01:11:19 -0300

    made it so the mod doesn't launch any of the scripts if repentogon isn't installed.

--- main.lua
+++ main.lua
@@ -1,5 +1,17 @@
 BeckyMod = RegisterMod("Becky", 1)
 
+local f = Font()
+f:Load("font/terminus.fnt")
+local function requireRepentogon()
+    if not REPENTOGON then
+        f:DrawString("REPENTOGON isn't installed", 60, 50, KColor(1,1,1,1,0,0,0),0,true)
+        f:DrawString("Head to the REPENTOGON mod page for instructions!", 60, 70, KColor(1,1,1,1,0,0,0),0,true)
+        f:DrawString("(Becky mod requires repentogon to work)", 60, 90, KColor(1,1,1,1,0,0,0),0,true)
+    end
+end
+BeckyMod:AddCallback(ModCallbacks.MC_POST_RENDER, requireRepentogon)
+if not REPENTOGON then return end
+
 --characters
 include("becky_scripts.characters.becky")
 
