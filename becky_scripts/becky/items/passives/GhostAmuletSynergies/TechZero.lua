bfd94768018cace3c1fe02cac79b514f0dfe41dc
AutisticGoat <smhkotry@gmail.com>
2025-10-28 18:39:28 -0700

    Fixed ghost shooting tech rays on hit

--- becky_scripts/becky/items/passives/GhostAmuletSynergies/TechZero.lua
+++ becky_scripts/becky/items/passives/GhostAmuletSynergies/TechZero.lua
@@ -2,21 +2,14 @@ local GHOST_BALL_VAR = Isaac.GetEntityVariantByName("Ghost Ball")
 
 ---@param familiar EntityFamiliar
 BeckyMod:AddCallback(BeckyMod.Callbacks.ON_GHOST_HIT_ENEMY, function (_, familiar, collider)
+    
 
     local player = familiar.Player
     local posDif = (familiar.Position - player.Position)
-    -- local laser 
-
-    -- if not player:HasCollectible(CollectibleType.COLLECTIBLE_TECHNOLOGY_ZERO) then return end
-
-    -- if laser then return end
     
-    print("a[sodk[aksd[pk]]]")
+    if not player:HasCollectible(CollectibleType.COLLECTIBLE_TECHNOLOGY_ZERO) then return end
 
     local laser = player:FireTechLaser(player.Position, LaserOffset.LASER_TECH1_OFFSET, posDif:Normalized(), false, true, player, 1) 
     laser:SetMaxDistance(posDif:Length())
-    print(laser.Variant)
-
-
     -- laser:SetMaxDistance(posDif:Length())
 end)
