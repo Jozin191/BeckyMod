6e3b5182d8647be57d506918b509b877c381ca7e
Ignatz-dev <131912906+Ignatz-dev@users.noreply.github.com>
2025-11-02 00:34:01 +0100

    Forgot to make it do damage, mb

--- becky_scripts/becky/items/actives/butcher's_cookbook.lua
+++ becky_scripts/becky/items/actives/butcher's_cookbook.lua
@@ -1,6 +1,7 @@
 local BUTCHERS_ID = Isaac.GetItemIdByName("Butcher's Cookbook")
 local SAW_VARIANT = Isaac.GetEntityVariantByName("Butcher's Cookbook Sawblade")
 local SAW_LIFETIME = 20 --in seconds
+local SAW_DPS = 20
 
 ---@param player EntityPlayer
 local function butchersUse(_, collectibleID, rngObj, player, useFlags, activeSlot, varData)
@@ -9,6 +10,8 @@ local function butchersUse(_, collectibleID, rngObj, player, useFlags, activeSlo
     local pos = room:GetGridPosition(gridIndex)
 
     local saw = BeckyMod.Game:Spawn(EntityType.ENTITY_EFFECT, SAW_VARIANT, pos, Vector.Zero, player, 0, 1)
+    saw:GetData().player = player
+
     Isaac.CreateTimer(function ()
         saw:Remove()
     end, SAW_LIFETIME * 30, 0, false)
@@ -19,6 +22,7 @@ BeckyMod:AddCallback(ModCallbacks.MC_USE_ITEM, butchersUse, BUTCHERS_ID)
 local function sawInit(_, effect)
     local sprite = effect:GetSprite()
     sprite:Play("Spawn")
+
 end
 BeckyMod:AddCallback(ModCallbacks.MC_POST_EFFECT_INIT, sawInit, SAW_VARIANT)
 
@@ -28,6 +32,12 @@ local function sawUpdate(_, effect)
 
     if sprite:IsFinished("Spawn") then
         sprite:Play("Idle")
+    end
+
+    for _, entity in ipairs(Isaac.GetRoomEntities()) do
+        if entity:IsVulnerableEnemy() and entity.Position:Distance(effect.Position) < 40 then
+            entity:TakeDamage(SAW_DPS/30, DamageFlag.DAMAGE_CLONES, EntityRef(effect:GetData(). player), 0)
+        end
     end
 end
 BeckyMod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, sawUpdate, SAW_VARIANT)
