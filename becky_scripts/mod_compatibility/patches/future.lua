035e58663bad30ac5267a9a267cd9aa7d4a4afe4
CVS-otherwise-Callie <notes2noah@gmail.com>
2025-11-03 20:49:46 -0500

    steven fix

--- becky_scripts/mod_compatibility/patches/future.lua
+++ becky_scripts/mod_compatibility/patches/future.lua
@@ -1,13 +1,15 @@
 local loader = BeckyMod.PatchesLoader
 
-if TheFuture then
-    TheFuture.ModdedCharacterDialogue["Becky"] = {
-        "Huh... i don't remember seeing you in the bible...",
-        "I guess you can go in.",
-        "As long as your buddy stops bouncing on my uvula...",
-		"...",
-        "...what? i do have an uvula"
-    }
+local function FuturePatch()
+    if TheFuture then
+        TheFuture.ModdedCharacterDialogue["Becky"] = {
+            "Huh... i don't remember seeing you in the bible...",
+            "I guess you can go in.",
+            "As long as your buddy stops bouncing on my uvula...",
+            "...",
+            "...what? i do have an uvula"
+        }
+    end
 end
 
 loader:RegisterPatch("Future", FuturePatch)
