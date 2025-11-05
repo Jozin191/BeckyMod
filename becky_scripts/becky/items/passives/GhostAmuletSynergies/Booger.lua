51bd3d18726a69416e4eb1d0100ca457617f15d3
Jozin191 <kirbydafaca@gmail.com>
2025-11-05 17:01:06 -0300

    Ported Synergies
    -Explosivo
    -Booger
    -Spore
    -Spirit Sword
    i also added TearSplit to the main.lua but Kotry said she's doing the rest

---
+++ becky_scripts/becky/items/passives/GhostAmuletSynergies/Booger.lua
@@ -0,0 +1,19 @@
+--local Callbacks = BeckyMod.Enums.Callbacks
+
+---@param fam EntityFamiliar
+---@param enemy EntityNPC
+BeckyMod:AddCallback(BeckyMod.Callbacks.ON_GHOST_HIT_ENEMY, function(_, fam, enemy)
+    local player = fam.Player
+
+    if not player then return end
+    if not (player:HasCollectible(CollectibleType.COLLECTIBLE_SINUS_INFECTION) or player:HasTrinket(TrinketType.TRINKET_NOSE_GOBLIN)) then return end
+
+    local rng = (player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_SINUS_INFECTION) or player:GetTrinketRNG(TrinketType.TRINKET_NOSE_GOBLIN))
+
+    if rng:RandomFloat() > 0.25 then return end
+
+    local tear = Isaac.Spawn(EntityType.ENTITY_TEAR, TearVariant.BOOGER, 0, enemy.Position, Vector.Zero, player):ToTear() ---@cast tear EntityTear
+    tear:AddTearFlags(TearFlags.TEAR_BOOGER)
+
+    -- local bomb = player:FireBomb(fam.Position, Vector.Zero, player)
+end)
