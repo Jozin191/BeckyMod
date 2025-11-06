c792300cf85f701ed74f8c307fcfc902009c2ed6
Jozin191 <kirbydafaca@gmail.com>
2025-11-05 22:58:52 -0300

    Future Compat Changes and Auri Compat
    the mod might give an error but it's because something is wrong in the patch loader
    Someone should fix that

--- becky_scripts/mod_compatibility/patches/future.lua
+++ becky_scripts/mod_compatibility/patches/future.lua
@@ -1,15 +1,14 @@
+local Mod = BeckyMod
 local loader = BeckyMod.PatchesLoader
 
 local function FuturePatch()
-    if TheFuture then
-        TheFuture.ModdedCharacterDialogue["Becky"] = {
+    TheFuture.ModdedCharacterDialogue["Becky"] = {
             "Huh... i don't remember seeing you in the bible...",
             "I guess you can go in.",
             "As long as your buddy stops bouncing on my uvula...",
             "...",
             "...what? i do have an uvula"
-        }
-    end
+    }
 end
 
-loader:RegisterPatch("Future", FuturePatch)
+loader:RegisterPatch("TheFuture", futurePatch)
