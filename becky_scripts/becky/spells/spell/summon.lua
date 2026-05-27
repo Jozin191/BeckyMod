96310218db3ed04c896b95e9c6e1f0d07b046525
rageTrash <119349103+rageTrash@users.noreply.github.com>
2026-05-26 21:48:21 -0300

    added multiple selection of familiars to summon spell (just to test)

--- becky_scripts/becky/spells/spell/summon.lua
+++ becky_scripts/becky/spells/spell/summon.lua
@@ -9,7 +9,20 @@ local ATTACKING_COOLDOWN = 7
 local PROJ_SPEED = Vector(15,0)
 
 local NULL_ITEM_ID = Isaac.GetNullItemIdByName("SPELL_Summon_BroberBobby")
+local NULL_ITEM_ID2 = Isaac.GetNullItemIdByName("SPELL_Summon_2")
+local NULL_ITEM_ID3 = Isaac.GetNullItemIdByName("SPELL_Summon_3")
+local NULL_ITEM_ID4 = Isaac.GetNullItemIdByName("SPELL_Summon_4")
 BeckyMod.Spells.NULL_ITEMS.SUMMON = NULL_ITEM_ID
+BeckyMod.Spells.NULL_ITEMS.SUMMON2 = NULL_ITEM_ID2
+BeckyMod.Spells.NULL_ITEMS.SUMMON3 = NULL_ITEM_ID3
+BeckyMod.Spells.NULL_ITEMS.SUMMON4 = NULL_ITEM_ID4
+
+local VALID_FAMS = {
+    [FamiliarVariant.BROTHER_BOBBY] = true,
+    [FamiliarVariant.LIL_HAUNT] = true,
+    [FamiliarVariant.PUNCHING_BAG] = true,
+    [FamiliarVariant.MULTIDIMENSIONAL_BABY] = true,
+}
 
 BeckyMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player)
     player:CheckFamiliar(
@@ -19,19 +32,46 @@ BeckyMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(_, player)
         nil,
         120
     )
+    player:CheckFamiliar(
+        FamiliarVariant.LIL_HAUNT,
+        player:GetEffects():GetNullEffectNum(NULL_ITEM_ID2),
+        player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_LIL_HAUNT),
+        nil,
+        120
+    )
+    player:CheckFamiliar(
+        FamiliarVariant.PUNCHING_BAG,
+        player:GetEffects():GetNullEffectNum(NULL_ITEM_ID3),
+        player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_PUNCHING_BAG),
+        nil,
+        120
+    )
+    player:CheckFamiliar(
+        FamiliarVariant.MULTIDIMENSIONAL_BABY,
+        player:GetEffects():GetNullEffectNum(NULL_ITEM_ID4),
+        player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_MULTIDIMENSIONAL_BABY),
+        nil,
+        120
+    )
 end, CacheFlag.CACHE_FAMILIARS)
 
 BeckyMod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, function(_, fam)
     if fam.SubType ~= 120 then return end
-    local color = fam:GetSprite().Color
-    color:SetColorize(1, 1, 1, 1)
-    color.A = 0.85
-    fam:GetSprite().Color = color
-end, FamiliarVariant.BROTHER_BOBBY)
+    if VALID_FAMS[fam.Variant] then
+        local color = fam:GetSprite().Color
+        color:SetColorize(1, 1, 1, 1)
+        color.A = 0.85
+        fam:GetSprite().Color = color
+    end
+end)
 
 
 local function fun(player)
-    --local data = BeckyMod.GetEntData(player)
+    local data = BeckyMod.GetEntData(player)
+    if data.MagicStaff_SelectSpellDir == nil then
+        data.MagicStaff_SelectSpellDir = { Type = BeckyMod.Spells.SpellType.SUMMON }
+        return
+    end
     --if data.MagicStaff_SelectSpellDir == nil then
     --    data.MagicStaff_SelectSpellDir = { Type = BeckyMod.Spells.SpellType.SUMMON, Choices = {
     --        [Direction.LEFT] = 2, --Shooting familiar
@@ -39,14 +79,34 @@ local function fun(player)
     --    } }
     --    return
     --end
+    local effects = player:GetEffects()
 
-    --if data.MagicStaff_SelectSpellDir.Dir == Direction.RIGHT then
-    --    player:UseActiveItem(CollectibleType.COLLECTIBLE_MR_ME, UseFlag.USE_MIMIC)
-    --else
+    if effects:HasNullEffect(NULL_ITEM_ID) then
+        effects:RemoveNullEffect(NULL_ITEM_ID, -1)
+    elseif effects:HasNullEffect(NULL_ITEM_ID2) then
+        effects:RemoveNullEffect(NULL_ITEM_ID2, -1)
+    elseif effects:HasNullEffect(NULL_ITEM_ID3) then
+        effects:RemoveNullEffect(NULL_ITEM_ID3, -1)
+    elseif effects:HasNullEffect(NULL_ITEM_ID4) then
+        effects:RemoveNullEffect(NULL_ITEM_ID4, -1)
+    end
+
+
+    if data.MagicStaff_SelectSpellDir.Dir == Direction.RIGHT then
+        player:AddNullItemEffect(NULL_ITEM_ID2)
+        --player:UseActiveItem(CollectibleType.COLLECTIBLE_MR_ME, UseFlag.USE_MIMIC)
+    elseif data.MagicStaff_SelectSpellDir.Dir == Direction.UP then
+        player:AddNullItemEffect(NULL_ITEM_ID3)
+    elseif data.MagicStaff_SelectSpellDir.Dir == Direction.DOWN then
+        player:AddNullItemEffect(NULL_ITEM_ID4)
+    else
         player:AddNullItemEffect(NULL_ITEM_ID)
-    --end
+    end
+    --local save = BeckyMod:RunSave(player)
+    --save.ManaCharge = save.ManaCharge - SPELL_COST
 
-    --data.MagicStaff_SelectSpellDir = nil
+
+    data.MagicStaff_SelectSpellDir = nil
 end
 
 local function canSelectFun(player, manaLeft)
@@ -57,7 +117,7 @@ return {
     BeckyMod.Spells.SpellType.SUMMON,
     Func = fun,
     CanSelect = canSelectFun,
-    Cost = 0
+    Cost = SPELL_COST
 }
 
 
