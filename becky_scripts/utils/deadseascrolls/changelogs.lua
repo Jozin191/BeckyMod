02b56e82e23cc9d3ab9da057a830dbe0e489a739
Jozin191 <190653238+Jozin191@users.noreply.github.com>
2026-08-10 17:08:46 -0300

    changelog changes i forgot to push

--- becky_scripts/utils/deadseascrolls/changelogs.lua
+++ becky_scripts/utils/deadseascrolls/changelogs.lua
@@ -1,9 +1,18 @@
 DeadSeaScrollsMenu.AddChangelog("Becky MOD", "Becky v2.0", [[{FSIZE2} 10/8/2026
 {FSIZE2}TAINTED + SYNERGIES UPDATE!!!
 
+Additions:
+-Tainted Becky added
+-Mouse control option on 
+RGON menu for base Becky
+-8 new unlockables
+-50+ new synergies for base Becky
+-1 new challenge
+
 Other changes:
 -Improve becky's deal mechanic
--Fix saguine feather granting fly when it shouldn't
+-Fix saguine feather 
+granting fly when it shouldn't
 -Added spanish EID
 -Minor tweaks to some items
 ]])
